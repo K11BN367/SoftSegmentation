9a83e9c2636a2269fde6029d293d7fe3639a9870
K11BN367 <alexander592beckmann@gmail.com>
2024-08-02 00:52:15 +0200

    commit

--- src/sample/'Method'1.jl
+++ src/sample/'Method'1.jl
@@ -1,7 +1,6 @@
 
 function Surrogates.QuasiMonteCarlo.sample(Hyperparameter_Array_Length::Integer, Lower_Bound::T1, Upper_Bound::T1, Sample_Algorithm::ShuffleSampleAlgorithm{Vector{T3}, N}) where {T1<:Union{Number, Union{Tuple{Vararg{T2}}, AbstractVector{<:T2}}}} where {T2, T3, N}
-    #Hyperparameter_Array = Array{T3, 2}(undef, (N, Hyperparameter_Array_Length))
-    Hyperparameter_Array = Array{NTuple{N, T3}, 1}(undef, Hyperparameter_Array_Length)
+    Hyperparameter_Array = Array{T3, 2}(undef, (N, Hyperparameter_Array_Length))
     Random_Array = Array{T3, 1}(undef, N)
     Length_Tuple = Sample_Algorithm.Hyperparameter_Array_Length_Tuple
     for j in 1:Hyperparameter_Array_Length
@@ -28,9 +27,8 @@ function Surrogates.QuasiMonteCarlo.sample(Hyperparameter_Array_Length::Integer,
             Random_Array[i] = Sample_Algorithm.Hyperparameter_Array_Tuple[i][Index]
         end
         #println(Random_Array)
-        #Hyperparameter_Array[:, j] = Random_Array
-        Hyperparameter_Array[j] = NTuple{N, T3}(Random_Array)
+        Hyperparameter_Array[:, j] = Random_Array
     end
+    println(size(Hyperparameter_Array))
     return Hyperparameter_Array
-    #return Tuple.(eachrow(Hyperparameter_Array))
 end
