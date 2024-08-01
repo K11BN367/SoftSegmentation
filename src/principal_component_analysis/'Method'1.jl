aa0b074139a15b88c6a9e9d4afb263873babb5d7
K11BN367 <alexander592beckmann@gmail.com>
2024-08-02 00:44:34 +0200

    commit

--- src/principal_component_analysis/'Method'1.jl
+++ src/principal_component_analysis/'Method'1.jl
@@ -1,5 +1,5 @@
 
-function principle_component_analysis(matrix::Array{T, 2}) where {T}
+function principal_component_analysis(matrix::Array{T, 2}) where {T}
     mean_vector = mean(matrix, dims=2)
     centered_matrix = matrix .- mean_vector
     covariance_matrix = cov(centered_matrix, dims=2)
